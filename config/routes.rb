965cd9e6e53c628acf6f57f9e2ff0641d38e1c79
gichuru <gichuru@gmail.com>
2017-03-03 18:41:27 +0300

    first commit for gigs

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+	resources :gigs
+	root 'gigs#index'
+end
---
+++ db/migrate/20170303094128_initial_schema.rb
@@ -0,0 +1,38 @@
+class InitialSchema < ActiveRecord::Migration[5.0]
+  def change
+  	create_table :gigs do |t|
+  	t.string 	:name
+  	t.text 		:description
+  	t.integer 	:budget
+  	t.date 		:startdate
+  	t.integer	:duration
+  	t.string 	:location
+  	t.boolean 	:open, default: true
+  	t.integer 	:hired
+  	t.timestamps
+  end
+
+  	create_table :categories do |t|
+  		t.string :name
+  	end
+
+  	create_table :skills do |t|
+  		t.string	:name
+  	end
+
+  	create_table :proposals do |t|
+  		t.integer	:bid
+  		t.text		:description
+  		t.timestamps
+  	end
+
+  	create_table :abilities do |t|
+  	end
+  	
+  	add_reference 	:gigs, :category, index: true
+  	add_reference 	:proposals, :gig, index: true
+  	add_reference	:abilities, :gig, index: true
+  	add_reference	:abilities, :skill, index: true 
+  end
+
+end
